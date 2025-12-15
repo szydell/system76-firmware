31fd2cf554891f7fe3b05921211f8526b70b8225
Marcin Szydelski <marcin@szydelscy.pl>
2025-12-15 14:46:09 +0100

    Get version from tag

--- fedora_copr_publish.sh
+++ fedora_copr_publish.sh
@@ -1,35 +1,39 @@
 #!/usr/bin/env bash
 #
-# Parse debian/changelog and set package version same as on PopOS! and Ubuntu
-# 
+# Get package version from upstream git tags and build for Fedora COPR
+#
 # ----------------------------------------------------------------------------
+# 2025-12-15 Marcin Szydelski
+#		switch to git tags as version source (debian/changelog no longer maintained)
 # 2021-01-09 Marcin Szydelski
 #		init
 
 # config
-export outdir="$(pwd)/.rpkg-build"
-
-# verification
-[ -f debian/changelog ] || { echo "No debian/changelog found."; exit 1; }
+outdir="$(pwd)/.rpkg-build"
+export outdir
 
 # fetch upstream
-git fetch upstream
+git fetch upstream || { echo "Failed to fetch upstream"; exit 1; }
+
 # merge
 git checkout master
 git merge upstream/master -m "fetch upstream" --log
 
+[ -d "${outdir}" ] && rm -rf "${outdir}"
 
-[ -d ${outdir} ] && rm -rf ${outdir}
+# Get latest version from upstream git tags (format: X.Y.Z)
+version=$(git tag -l --sort=-v:refname | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | head -1)
 
-version_in_changelog=$(grep -E "system76-firmware \([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+" debian/changelog | head -1)
-_tmp=${version_in_changelog%%)*}
-_tmp=${_tmp%%~*}
-version=${_tmp##*\(}
+if [ -z "$version" ]; then
+	echo "No valid version tag found in upstream"
+	exit 1
+fi
 
-_tmp=$(git tag --list system76-firmware-"$version"-'*' | sort -n -t '-' -k 4 -r | head -1)
+# Get our latest release number for this version
+_tmp=$(git tag --list "system76-firmware-${version}-"'*' | sort -t '-' -k 4 -n -r | head -1)
 release=${_tmp##*-}
 
-if [ "z$release" == "z" ]; then
+if [ -z "$release" ]; then
 	release=1
 else
 	if ! [[ "$release" =~ ^[0-9]+$ ]]; then
@@ -40,17 +44,19 @@ else
 	((release++))
 fi
 
+echo "Building version: ${version}-${release}"
+
 # as a workaround set static version in spec file
-sed -i "s#^Version:    .*#Version:    $version#" system76-firmware.spec.rpkg
-sed -i "s#^Release:    .*#Release:    $release#" system76-firmware.spec.rpkg
-git commit -m"bump Version to: $version-$release" system76-firmware.spec.rpkg
+sed -i "s#^Version:    .*#Version:    ${version}#" system76-firmware.spec.rpkg
+sed -i "s#^Release:    .*#Release:    ${release}#" system76-firmware.spec.rpkg
+git commit -m "bump Version to: ${version}-${release}" system76-firmware.spec.rpkg
 
 #test & build srpm
 mkdir "$outdir"
 rpkg local --outdir="$outdir" || { echo "rpkg local failed"; exit 4; }
 
 # rpkg tag
-rpkg tag --version="$version"  --release="$release"
+rpkg tag --version="${version}" --release="${release}"
 
 srpm="$(ls .rpkg-build/system76-firmware-*.src.rpm)"
 
